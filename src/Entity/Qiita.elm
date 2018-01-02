77e1ce7047771d34d68d9c33a694d2d517ed931f
k-motoyan <k.motoyan888@gmail.com>
2018-01-02 23:02:43 +0900

    Added type: User to Entity.Qiita module.

--- src/Entity/Qiita.elm
+++ src/Entity/Qiita.elm
@@ -1,1 +1,20 @@
-module Entity.Qiita exposing (..)
+module Entity.Qiita exposing (User)
+
+
+type alias User =
+    { description: String
+    , facebook_id: String
+    , followees_count: Int
+    , followers_count: Int
+    , github_login_name: String
+    , id: String
+    , items_count: Int
+    , linkedin_id: String
+    , location: String
+    , name: String
+    , organization: String
+    , permanent_id: Int
+    , profile_image_url: String
+    , twitter_screen_name: String
+    , website_url: String
+    }
