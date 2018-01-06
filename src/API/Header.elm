bb6b28216aee6f0c066084aa60cac21f5594d055
k-motoyan <k.motoyan888@gmail.com>
2018-01-06 22:02:46 +0900

    Appended Content-Type documentation to API.Header module.

--- src/API/Header.elm
+++ src/API/Header.elm
@@ -1,8 +1,13 @@
 module API.Header exposing
-    ( ContentType(..)
-    , contentTypeHeader
+    ( ContentType(..) , contentTypeHeader
     )
 
+{-| This module provides Http request header utilities.
+
+# Content-Type
+@docs ContentType, contentTypeHeader
+
+-}
 
 import Http exposing (Header, header)
 
@@ -10,10 +15,19 @@ import Http exposing (Header, header)
 -- ContentType
 
 
+{-| Content-Types.
+-}
 type ContentType
     = ApplicationJson
 
 
+{-| Create content type header.
+
+Usage:
+
+    >>> contentTypeHeader ApplicationJson
+    header "Content-Type" "application/json"
+-}
 contentTypeHeader : ContentType -> Header
 contentTypeHeader contentType =
     header "Content-Type" (contentTypeToString contentType)
