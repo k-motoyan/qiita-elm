4ea489e243982a9b24d4277c46a9d8bb621a5516
k-motoyan <k.motoyan888@gmail.com>
2018-01-08 21:14:43 +0900

    Implemented HomePage Model.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -1,9 +1,31 @@
-module Page.Home exposing (view)
+module Page.Home exposing (Model, initModel, view)
 
 
 import Html exposing (Html, h1, text)
+import Entity.Qiita exposing (Item)
 
 
-view : Html msg
-view =
+-- Model
+
+
+type alias Model =
+    { isLoading: Bool
+    , isError: Bool
+    , items: Maybe (List Item)
+    }
+
+
+initModel : Model
+initModel =
+    { isLoading = False
+    , isError = False
+    , items = Nothing
+    }
+
+
+-- View
+
+
+view : Model -> Html msg
+view model =
     h1 [] [ text "Home" ]
