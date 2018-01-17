14d0ec247cbfd9c9240f87c27f9fb97a065cf1a4
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2018-01-17 22:41:11 +0900

    Wrote document: decodeItem of Json.Qiita.Decoder module.

--- src/Json/Qiita/Decoder.elm
+++ src/Json/Qiita/Decoder.elm
@@ -175,6 +175,94 @@ decodeItems =
     list decodeItem
 
 
+{-| Qiita item object decoder.
+
+Usage:
+
+    >>> Json.Decode.decodeString
+    >>>     decodeItem
+    >>>     """
+    >>>         {
+    >>>             "rendered_body": "<h1>Example</h1>",
+    >>>             "body": "# Example",
+    >>>             "comments_count": 100,
+    >>>             "created_at": "2000-01-01T00:00:00+00:00",
+    >>>             "id": "4bd431809afb1bb99e4f",
+    >>>             "likes_count": 100,
+    >>>             "private": false,
+    >>>             "reactions_count": 100,
+    >>>             "tags": [
+    >>>                 {
+    >>>                     "name": "Ruby",
+    >>>                     "versions": [
+    >>>                         "0.0.1"
+    >>>                     ]
+    >>>                 }
+    >>>             ],
+    >>>             "title": "Example title",
+    >>>             "updated_at": "2000-01-01T00:00:00+00:00",
+    >>>             "url": "https://qiita.com/yaotti/items/4bd431809afb1bb99e4f",
+    >>>             "user": {
+    >>>                 "description": "Hello, world.",
+    >>>                 "facebook_id": "yaotti",
+    >>>                 "followees_count": 100,
+    >>>                 "followers_count": 200,
+    >>>                 "github_login_name": "yaotti",
+    >>>                 "id": "yaotti",
+    >>>                 "items_count": 300,
+    >>>                 "linkedin_id": "yaotti",
+    >>>                 "location": "Tokyo, Japan",
+    >>>                 "name": "Hiroshige Umino",
+    >>>                 "organization": "Increments Inc",
+    >>>                 "permanent_id": 1,
+    >>>                 "profile_image_url": "https://si0.twimg.com/profile_images/2309761038/1ijg13pfs0dg84sk2y0h_normal.jpeg",
+    >>>                 "twitter_screen_name": "yaotti",
+    >>>                 "website_url": "http://yaotti.hatenablog.com"
+    >>>             }
+    >>>         }
+    >>>     """
+    Result.Ok
+        { rendered_body = "<h1>Example</h1>"
+        , body = "# Example"
+        , comments_count = 100
+        , created_at =
+            case Date.fromString "2000-01-01T00:00:00+00:00" of
+                Ok value ->
+                    value
+                Err e ->
+                    Debug.crash "Date format error" e
+        , id = "4bd431809afb1bb99e4f"
+        , likes_count = 100
+        , private = False
+        , reactions_count = 100
+        , tags = [ { name = "Ruby", versions = [ "0.0.1" ] } ]
+        , title = "Example title"
+        , updated_at =
+            case Date.fromString "2000-01-01T00:00:00+00:00" of
+                Ok value ->
+                    value
+                Err e ->
+                    Debug.crash "Date format error" e
+        , url = "https://qiita.com/yaotti/items/4bd431809afb1bb99e4f"
+        , user =
+            { description = Just "Hello, world."
+            , facebook_id = Just "yaotti"
+            , followees_count = 100
+            , followers_count = 200
+            , github_login_name = Just "yaotti"
+            , id = "yaotti"
+            , items_count = 300
+            , linkedin_id = Just "yaotti"
+            , location = Just "Tokyo, Japan"
+            , name = "Hiroshige Umino"
+            , organization = Just "Increments Inc"
+            , permanent_id = 1
+            , profile_image_url = "https://si0.twimg.com/profile_images/2309761038/1ijg13pfs0dg84sk2y0h_normal.jpeg"
+            , twitter_screen_name = Just "yaotti"
+            , website_url = Just "http://yaotti.hatenablog.com"
+            }
+        }
+-}
 decodeItem : Decoder Item
 decodeItem =
     decode Item
