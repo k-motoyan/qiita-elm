990aa7ca6f0db047f4a4d1f25e53a23b5b024354
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2018-01-22 15:42:52 +0900

    Added comment count design to home item list.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -4,13 +4,14 @@ module Page.Home exposing (Model, initModel, Msg(..), update, view)
 import Html exposing (..)
 import Html.Attributes exposing (class, style, src, alt, href)
 import Html.Events exposing (onClick)
-import Color exposing (red)
+import Color exposing (red, darkGrey)
 import Http
 import Http.Request.Qiita exposing (getItems)
 import Route exposing (Route(..), Slug(..))
 import Entity.Qiita exposing (Item, User)
 import Views.LoadingIndicator as LoadingIndicator
 import Material.Icons.Action as ActionIcon
+import Material.Icons.Communication as CommunicationIcon
 
 
 -- Model
@@ -106,6 +107,7 @@ listItemView item =
                     , nav [ class "level is-mobile" ]
                         [ div [ class "level-left"]
                             [ qiitaLikeIcon item
+                            , qiitaCommentIcon item
                             ]
                         ]
                     ]
@@ -118,10 +120,33 @@ userImage user =
     img [ src user.profile_image_url, alt "image" ] []
 
 
+iconSize : Int
+iconSize = 18
+
+
+iconStyle : Attribute msg
+iconStyle =
+    style
+        [ ("margin-left", "4px")
+        ]
+
+
+countText : Int -> Html msg
+countText count =
+    text (count |> toString)
+
+
 qiitaLikeIcon : Item -> Html msg
 qiitaLikeIcon item =
     span [ class "level-item" ]
-        [ ActionIcon.favorite red 16
-        , span [ style [ ("margin-left", "4px") ] ]
-            [ text (item.likes_count |> toString) ]
+        [ ActionIcon.favorite_border red iconSize
+        , span [ iconStyle ] [ countText item.likes_count ]
+        ]
+
+
+qiitaCommentIcon : Item -> Html msg
+qiitaCommentIcon item =
+    span [ class "level-item" ]
+        [ CommunicationIcon.chat_bubble_outline darkGrey iconSize
+        , span [ iconStyle ] [ countText item.reactions_count ]
         ]
