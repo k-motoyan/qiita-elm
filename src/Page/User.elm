75e40a6f82f2ba504704a3dfc486208cd4afb451
k-motoyan <k.motoyan888@gmail.com>
2018-02-14 21:49:17 +0900

    Implemented user page model.

--- src/Page/User.elm
+++ src/Page/User.elm
@@ -1,12 +1,27 @@
-module Page.User exposing (view)
+module Page.User exposing (Model, view)
 
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
 import Route exposing (Route(..))
 import Model exposing (PageState(Found))
 import Update exposing (Msg(..))
+import Entity.Qiita exposing (User, StockItem)
 import Views.Tabs as Tabs exposing (TabItem)
 import Views.LoadingIndicator as LoadingIndicator
+
+
+-- Model
+
+
+type alias Model =
+    { my: User
+    , stockItems: Maybe (List StockItem)
+    , followers: Maybe (List User)
+    , followees: Maybe (List User)
+    }
+
+
+-- View
 
 
 tabItems : List (TabItem)
